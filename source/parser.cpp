f4c79cc3e78a41678a306d88a6a2dc5e9f5160bd
Dan Tracy <djt5019@gmail.com>
2010-04-05 15:01:43 -0400

    modified the parser file

--- source/parser.cpp
+++ source/parser.cpp
@@ -8,14 +8,7 @@
 using namespace std;
 
 #define RULEFILE "rulefile.txt"
-const string AND =  "&&";
-const string OR  =  "||";
-
-struct knowledge
-{
-    bool state;
-    string name;
-};
+#define KNOWLEDGEFILE "knowledge.txt"
 
 struct rule
 {
@@ -24,8 +17,8 @@ struct rule
     string consequent;
 };
 
-ifstream ruleBaseInput;
-map<int, struct knowledge> knowledgeBase;
+
+map<string, bool> knowledgeBase;
 map<int, struct rule> ruleBase;
 
 //! Rules will follow the form of IF <expr1> AND/OR <expr2> ... AND/OR <exprN> THEN consequent
@@ -37,7 +30,14 @@ void initalizeExpertSystem()
 {
     //! Will read from the rule file, create a structure for each containing the consequent
     //! and the list of antecedents.
+    ifstream input;
     
+    initRules( input );
+    initKnowledge( input);
+}
+
+void initRules(ifstream& ruleBaseInput)
+{    
     int index = 0;
     string line;
     string temp;
@@ -48,14 +48,11 @@ void initalizeExpertSystem()
     struct rule tempRule;
     
     ruleBaseInput.open( RULEFILE );
-    
     if( !ruleBaseInput.is_open() )
     {
-	printf("%s Error: The Rule File could not be opened\n", __func__);
+	fprintf(stderr,"%s Error: The Rule File could not be opened\n", __func__);
 	exit(EXIT_FAILURE);
     }
-    
-
     
     while( !ruleBaseInput.eof() )
     {
@@ -67,14 +64,9 @@ void initalizeExpertSystem()
 	    string junk;
 	    getline( ruleBaseInput, junk );
 	    junk.clear();
+	    continue;
 	}
-	if( temp == "AND" || temp == "and" )
-	   temp = "&&";
-	else if (temp == "OR" || temp == "or" )
-	   temp = "||";
-	else if (temp == "NOT" || temp == "not")
-	   temp = "!";
-	
+
 	if( temp == "IF" || temp == "if" ) 
 	{
 	    antecedentFlag = true;
@@ -97,7 +89,13 @@ void initalizeExpertSystem()
 	  
 	if( temp.find(';') != string::npos ) //!Found a semicolon delimiter 
 	{
-	    ruleBase.insert( std::pair<int,struct rule>(++index,tempRule));
+	    ++index;
+	    
+	    if( tempRule.antecedent.size() % 2 != 1 )
+	    {
+		fprintf(stderr,"%s Warning: Rule %d may be malformed\n", __func__, index);
+	    }
+	    ruleBase.insert( std::pair<int,struct rule>(index,tempRule));
 	    line.clear();
 	    tempRule.antecedent.clear();
 	}
@@ -107,7 +105,31 @@ void initalizeExpertSystem()
     ruleBaseInput.close();
 }
 
-bool triggerRule(struct rule theRule)
+void initKnowledge(ifstream& knowledgeInput)
+{
+    string name;
+    bool state;
+    
+    knowledgeInput.open( KNOWLEDGEFILE );
+    
+    if( !knowledgeInput.open() )
+    {
+       fprintf(stderr, "%s Error: Couldn't open the file %s\n",__func__, KNOWLEDGEFILE );
+       exit(EXIT_FAILURE);
+    }
+    
+    while( !knowledgeInput.eof() )
+    {
+	knowledgeInput >> name;
+	knowledgeInput >> state;
+	knowledgeBase.insert( std::pair<string, bool>(name,state) );
+	name.clear();
+    }
+    
+    knowledgeInput.close();
+}
+
+bool evaluateRule(struct rule theRule)
 {
     //! This will take a function then parse it and proceed to call the
     //! following functions or poll the knowledgeBase.  If something is 
@@ -117,19 +139,77 @@ bool triggerRule(struct rule theRule)
     //! times to find a good rule to apply to the game.
     
     struct rule rules;
-    struct knowledge fact;
+    bool fact;
+    
+    bool (*function)(void) = NULL;
+    bool expressionValue = false;
+    
+    if( theRule.antecedent.size() < 2 )
+    {
+	//There is only one expression
+	expressionValue = 
+	
+    }
+    
+    vector<string>::iterator prevItr = theRule.antecedent.begin() + 1;
+    vector<string>::iterator currentItr = theRule.antecedent.begin() + 2;
+    
+    while( currentItr != theRule.antecedent.end() )
+    {
+	string tempString = *currentItr;
+	string op  = *prevItr;
+	bool result;
+	
+	if( tempString.find("()") != string::npos )
+	{
+	    //TODO: Add the functions here 
+	    // Call the function, save the return value
+	    // then use the operator and 
+	}
+	else
+	{
+	  //It's not a function so look it up in the knowledge base    
+	  map<string,bool>::iterator itr = knowledgeBase.find( tempString );
+	  
+	  if( itr == map::end() )
+	  {
+	      //knowledge doesnt exist in the database so try to prove it
+	      result = searchForRule( tempString );
+	  }
+	  
+	}
+	
+	if( op == "AND" || op == "and" )
+	{
+	}
+	else if( tempString == "OR" || tempString == "or" )
+	{
+	}
+	else if( tempString == "NOT" || tempString == "not" )
+	{
+	}
+	
+	prevItr    += 2;
+	currentItr += 2;
+    }
     
     
-    return false;
+    return expressionValue; 
+}
+
+bool searchForRule( string rulename )
+{
+    //! This will search for the rulename in the list of consequents stored in the
+    //! rulebase in an effort to prove wether it is true or false
+    
     
 }
-#ifdef DEBUG
 
+#ifdef DEBUG
 int main()
 {
   initalizeExpertSystem();
   return 0;
 }
-
 #endif
 
